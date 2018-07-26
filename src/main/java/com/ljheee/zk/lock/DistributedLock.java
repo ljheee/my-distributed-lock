9d1cb735a52391fbd7eaf1f871fa28556e10538a
lijianhua04 <lijianhua04@meituan.com>
2018-07-26 22:06:13 +0800

    add readme

--- src/main/java/com/ljheee/zk/lock/DistributedLock.java
+++ src/main/java/com/ljheee/zk/lock/DistributedLock.java
@@ -20,6 +20,7 @@ public class DistributedLock {
     private String connectString = "192.168.1.200:2181/myTest";
 
 
+    // 跟节点
     private static String PARENT_PATH = "dlocks";
 
     public DistributedLock() {
@@ -44,10 +45,12 @@ public class DistributedLock {
 
 
     public String getLock(String itemId) throws KeeperException, InterruptedException {
+
+        // 检查 跟节点
         Stat exists = zookeeper.exists(PARENT_PATH, false);
         if (exists == null) {
             // 创建分布式锁 持久化跟节点
-            zookeeper.create(PARENT_PATH, "This is distribute lock parent path.".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            zookeeper.create(PARENT_PATH, "This is distribute lock root path.".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         }
 
         //
@@ -60,7 +63,7 @@ public class DistributedLock {
 
 
         // 创建 唯一标识的子节点
-        String lock_path = id_path + "/lock_";
+        String lock_path = id_path + "/lock_";//  want/lock_0000000000 want/lock_0000000001
 
         String current_path = zookeeper.create(id_path, "This is distribute lock".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
 
@@ -68,21 +71,23 @@ public class DistributedLock {
         List<String> children = zookeeper.getChildren(current_path, false);
 
         if (children.size() == 1) {
-            // ok
-            return id_path;// todo children.get(0)
+            // ok 你是第一个来创建EPHEMERAL_SEQUENTIAL的，直接获得锁，序号路径即为current_path
+            return current_path;
         }
 
-        // 开启监听，监听带序号的  上一个
-        String current_path_id = current_path.substring(current_path.indexOf("lock_") + 5);
-        int pre_id = Integer.parseInt(current_path_id) - 1;
+        // 开启监听，监听带序号的 上一个节点（因为按客户端请求顺序，也是创建的带递增序号的临时节点，你什么时候能拿到锁，取决于你前面一个人什么时候结束）
+        String current_path_id = current_path.substring(current_path.indexOf("lock_") + 5);// 截取获得当前节点序号
+        int pre_id = Integer.parseInt(current_path_id) - 1;//上一个节点序号
 
 
-        String pre_path = id_path + "/lock_" + String.format("%010d", pre_id);
+        String pre_path = id_path + "/lock_" + String.format("%010d", pre_id);//上一个节点路径
+
+        // 监听上一个节点路径（结束事件）
         zookeeper.exists(pre_path, new Watcher() {
             public void process(WatchedEvent watchedEvent) {
 
                 if (watchedEvent.getType() == Event.EventType.NodeDeleted) {
-                    // ok
+                    // ok 轮到你了，现在获得锁了
                     countDownLatch.countDown();
                 }
             }
